b773170735058ce4d0030023cd97e81c41ca2e45
isushkevich <ivan.sushik@gmail.com>
2020-04-17 01:39:57 +0300

    update workers' names

--- script.js
+++ script.js
@@ -43,69 +43,69 @@ const employees = [
     {
         dept_unit_id: 0,
         id: 0,
-        name: "YarikHead",
+        name: "Richard",
         tel: "123-123-3",
         salary: 3000
     },
     {
         id: 1,
-        name: "MashaLead",
+        name: "Gordon",
         dept_unit_id: 1,
         tel: "123-123-3",
         salary: 2000
     },
     {
         id: 2,
-        name: "SashaLead",
+        name: "Michele",
         dept_unit_id: 1,
         tel: "123-123-3",
         salary: 2200
     },
     {
         id: 3,
-        name: "MirraDev",
+        name: "Greta",
         dept_unit_id: 2,
         tel: "123-123-3",
         salary: 1200
     },
     {
         id: 4,
-        name: "IraDev",
+        name: "Jeremy",
         dept_unit_id: 2,
         tel: "123-123-3",
         salary: 1000
     },
     {
         id: 5,
-        name: "DanikHead3",
+        name: "Elvis",
         dept_unit_id: 3,
         tel: "123-123-33",
         salary: 3000
     },
     {
         id: 6,
-        name: "OliaLead3",
+        name: "Julie",
         dept_unit_id: 4,
         tel: "123-123-3",
         salary: 2200
     },
     {
         id: 7,
-        name: "KoliaLead",
+        name: "Ronald",
         dept_unit_id: 4,
         tel: "123-123-3",
         salary: 2000
     },
     {
         id: 8,
-        name: "LenaTest",
+        name: "Patrick",
         dept_unit_id: 5,
         tel: "123-123-3",
         salary: 1200
     },
     {
         id: 9,
-        name: "SienaTest",
+        name: "Debbie",
         dept_unit_id: 5,
         tel: "123-123-3",
         salary: 1000
@@ -154,7 +154,6 @@ let developer2 = {
     id: 20,
     parent_id: 1,
 };
-
 let qaDeptHead = {
     name: 'Quality Assurance Management',
     id: 3,
